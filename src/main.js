be2cdc7237f72600a3957d78c98201572b1f2e32
sunyaping950306 <1250823197@qq.com>
2019-07-10 22:12:49 +0800

    创建项目初始目录

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 职责：依赖项目需要的资源（js模块 css资源...）
+// 职责2：实例化根实例  render()渲染APP组件  然后挂载到#app的容器内容
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
