95fb3f9708fcb1028677143321e1ed23fe0fd1d1
sunyaping950306 <1250823197@qq.com>
2019-07-10 23:59:53 +0800

    登录模块基础布局及绘制表单

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 // 创建路由对象 及配置路由规则 和其他路由配置
 import VueRouter from 'vue-router'
 import Vue from 'vue'
+import Login from '@/views/login'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: []
+  routes: [
+    { name: 'login', path: '/login', component: Login }
+  ]
 })
 
 export default router
